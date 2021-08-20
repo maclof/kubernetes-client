947c6c891fc8758ca5d829198571d3c0373c0594
Danijel K <dkralj@gmail.com>
2021-08-20 12:43:28 +0200

    Fix return annotion for exec
    Response can be an array depending on what 'std*' input you give it, this fixes phpstan.

--- src/Repositories/PodRepository.php
+++ src/Repositories/PodRepository.php
@@ -31,7 +31,7 @@ class PodRepository extends Repository
 	 *
 	 * @param  \Maclof\Kubernetes\Models\Pod $pod
 	 * @param  array $options
-	 * @return string
+	 * @return string|array
 	 */
 	public function exec(Pod $pod, array $options = [])
 	{
