44689a69608f6bcb30ba484ee7880ffa82b0dfc9
Michael Butler <mbutler@tumblr.com>
2021-11-11 12:49:17 -0500

    Fix unit tests and re-implement test for get pods API call

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,73 +1,96 @@
 <?php
 
+use Http\Client\Common\HttpMethodsClient;
 use Maclof\Kubernetes\Client;
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Message\RequestInterface;
-use GuzzleHttp\Message\ResponseInterface;
+use Maclof\Kubernetes\Collections\PodCollection;
+use Maclof\Kubernetes\Models\Pod;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
+use Psr\Http\Client\ClientInterface;
 
 class ClientTest extends TestCase
 {
-	protected function getClient(GuzzleClient $guzzleClient = null)
+	public function testSendRequestJsonParsesResponse()
 	{
-		return new Client(
-			[
-				'master' => '',
+		$httpClientProp = new ReflectionProperty(Client::class, 'httpClient');
+		$httpClientProp->setAccessible(true);
+		$client = new Client([
+			'master' => 'https://api.example.com',
+		]);
+
+		$mockClientInterface = $this->getMockBuilder(ClientInterface::class)
+			->setMethods(['sendRequest'])
+			->getMock();
+
+		$jsonBody = json_encode([
+			'message' => 'Hello world',
+		]);
+
+		$response = new Response(200, [], $jsonBody);
+
+		$mockClientInterface->expects($this->once())
+			->method('sendRequest')
+			->withAnyParameters()
+			->willReturn($response);
+
+		$httpClient = new HttpMethodsClient($mockClientInterface, new Psr17Factory());
+
+		$httpClientProp->setValue($client, $httpClient);
+
+		$result = $client->sendRequest('GET', '/v1/poddy/');
+
+		$this->assertSame([
+			'message' => 'Hello world',
+		], $result);
+	}
+
+	private function setMockHttpResponse(
+		Client $client,
+		array $mockResponse,
+		array $expectedSendArgs,
+		int $respStatusCode = 200,
+		array $respHeaders = []
+	) {
+		$httpClientProp = new ReflectionProperty(Client::class, 'httpClient');
+		$httpClientProp->setAccessible(true);
+
+		$mockHttpMethodsClient = $this->getMockBuilder(HttpMethodsMockClient::class)
+			->setMethods(['send'])
+			->getMock();
+
+		$jsonBody = json_encode($mockResponse);
+
+		$response = new Response($respStatusCode, $respHeaders, $jsonBody);
+
+		$mockHttpMethodsClient->expects($this->once())
+			->method('send')
+			->with(...$expectedSendArgs)
+			->willReturn($response);
+
+		$httpClientProp->setValue($client, $mockHttpMethodsClient);
+	}
+
+	public function testGetPodsFromApi()
+	{
+		$client = new Client();
+
+		$jsonBody = [
+			'items' => [
+				[],
+				[],
+				[],
 			],
-			$guzzleClient
-		);
+		];
+
+		$this->setMockHttpResponse($client, $jsonBody, ['GET', '/api/' . $this->apiVersion . '/namespaces/' . $this->namespace . '/pods']);
+
+		$result = $client->pods()->find();
+
+		$this->assertInstanceOf(PodCollection::class, $result);
+
+		$this->assertSame(3, $result->count());
+
+		$pod1 = $result->first();
+		$this->assertInstanceOf(Pod::class, $pod1);
 	}
-
-	protected function getMockGuzzleCLient(RequestInterface $guzzleRequest = null, ResponseInterface $guzzleResponse = null)
-	{
-		$mockGuzzleClient = Mockery::mock('GuzzleHttp\Client');
-
-		if ($guzzleRequest && $guzzleResponse) {
-			$mockGuzzleClient->shouldReceive('send')->with($guzzleRequest)->andReturn($guzzleResponse);
-		}
-
-		return $mockGuzzleClient;
-	}
-
-	protected function getMockGuzzleRequest()
-	{
-		$mockGuzzleRequest = Mockery::mock('GuzzleHttp\Message\RequestInterface');
-
-		return $mockGuzzleRequest;
-	}
-
-	protected function getMockGuzzleResponse(array $response = array())
-	{
-		$mockGuzzleResponse = Mockery::mock('GuzzleHttp\Message\ResponseInterface');
-		$mockGuzzleResponse->shouldReceive('json')->with()->andReturn($response);
-
-		return $mockGuzzleResponse;
-	}
-
-	public function test_get_guzzle_client()
-	{
-		$client = $this->getClient();
-
-		$this->assertInstanceOf('GuzzleHttp\Client', $client->getGuzzleClient());
-	}
-
-	// public function test_get_pods()
-	// {
-	// 	$request = $this->getMockGuzzleRequest();
-	// 	$response = $this->getMockGuzzleResponse([
-	// 		'items' => [
-	// 			[],
-	// 			[],
-	// 			[],
-	// 		],
-	// 	]);
-	// 	$mockGuzzleClient = $this->getMockGuzzleCLient($request, $response);
-	// 	$mockGuzzleClient->shouldReceive('createRequest')
-	// 		->with('GET', '/api/' . $this->apiVersion . '/namespaces/' . $this->namespace . '/pods', ['query' => [], 'body' => null])
-	// 		->andReturn($request);
-
-	// 	$client = $this->getClient($mockGuzzleClient);
-	// 	$pods = $client->pods()->find();
-
-	// 	$this->assertInstanceOf('Maclof\Kubernetes\Collections\PodCollection', $pods);
-	// }
 }
